bdc35ae9eaf06bc52ce7aa7ba810c3185cf46c61
Ryan Huynh <ryanhuynh1803@gmail.com>
2025-10-31 23:03:44 +0700

    Reverted all changes back to version c0364e94335abb02b7f767050d078d3a37028240

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import FacebookPixel from './components/FacebookPixel';
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Sonner />
       <BrowserRouter>
+        <FacebookPixel />
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
